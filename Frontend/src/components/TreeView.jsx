c5ea41616300d715cd44024b68a4867adf577b00
JantonaE <jesusantonaespejo@uma.es>
2024-04-08 11:14:47 +0200

    TreeView Improvement 1

--- Frontend/src/components/TreeView.jsx
+++ Frontend/src/components/TreeView.jsx
@@ -24,7 +24,14 @@ function Dropdown({menuData}) {
             {subMenu.map((subItem, index) => (
               <li key={index} onClick={toggleSubMenu}>
                 {subItem.label}
-                <a href={`/Company/PPPO/Sons?parentId=${subItem.parentId}`}>+</a>
+                <a className="inline-block align-middle" href={`/Company/PPPO/Sons?parentId=${subItem.parentId}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.25} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 15l6 -6" />
+                  <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                  <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                </svg>
+                </a>
                 {subItem.submenu && renderSubMenu(subItem.submenu)}
               </li>
             ))}
@@ -36,11 +43,16 @@ function Dropdown({menuData}) {
         <div>
           <ul>
             {menuData.map((item, index) => (
-              <div className="flex">
+              <div className="flex items-center">
               <li key={index} onClick={toggleSubMenu}>
-                {item.label}
-                <a href={`/Company/PPPO/Sons?parentId=${item.parentId}`}>
-                  +
+               \/ {item.label} 
+                <a className="inline-block align-middle" href={`/Company/PPPO/Sons?parentId=${item.parentId}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.25} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 15l6 -6" />
+                  <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
+                  <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
+                </svg>
                 </a>
                 {item.submenu && renderSubMenu(item.submenu)}
               </li>
