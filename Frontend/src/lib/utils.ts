3da20f0c7c167cd42c1cedff39e89f359cf64bde
JantonaE <jesusantonaespejo@uma.es>
2024-06-03 10:51:44 +0200

    hash Psw

--- Frontend/src/lib/utils.ts
+++ Frontend/src/lib/utils.ts
@@ -62,3 +62,11 @@ export function formatDateFilter(inputDate) {
     return result;
 }
 
+export async function hashPassword(password) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashedPassword = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    return hashedPassword;
+  }
